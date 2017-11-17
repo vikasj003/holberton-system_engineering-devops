9aabc4b5638d8cb05d0f3c9d963a42e544bcb98b
wendysegura <118@holbertonschool.com>
2017-11-16 20:10:40 -0800

    Update 1-install_a_package.pp

--- 0x0D-configuration_management/1-install_a_package.pp
+++ 0x0D-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
 #Using Puppet, install puppet-lint
 
-package {'puppet-lint':
-  ensure => installed,
-  require => '2.1.1',
-  provider => gem,
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
 
