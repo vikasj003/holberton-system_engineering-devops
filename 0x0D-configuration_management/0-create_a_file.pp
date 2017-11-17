af0daad7f07f688caa5bca715d67d85273a863e9
wendysegura <118@holbertonschool.com>
2017-11-16 20:05:45 -0800

    Update 0-create_a_file.pp

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 #Using Puppet, create a file in /tmp
 
 file { '/tmp/holberton':
-  mode => 0744,
+  ensure => file,
+  path => '/tmp/holberton',
+  mode => '0744',
   owner => 'www-data',
   group => 'www-data',
   content => 'I love Puppet'
