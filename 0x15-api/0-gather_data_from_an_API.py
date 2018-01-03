4c239122e7c95fbbb566a380dd73580083a5b686
wendysegura <118@holbertonschool.com>
2018-01-03 03:15:49 +0000

    upload

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API"""
+import sys
+import json
+import requests
+
+
+if __name__ == "__main__":
+    employee_id = {'id': argv[1]}
+    url_user = 'https://jsonplaceholder.typicode.com/users'
+
+    taks_id = {'userId': argv[1]}
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
+
+    #employee data request
+    employee_req = requests.get(url_user + employee_id)
+    employee = employee_req.json()
+
+    #task todo request
+    list_tasks = []
+    
+    todos_req = requests.get(url_todos + task_id)
+    todos = todos_req.json()
+
+    #grab each task and add to list
+    for item in todos:
+        if item.get('completed') is True:
+            list_tasks.append(item.get('title'))
+
+    number_tasks = len(list_tasks)
+    total_tasks = len(todos)
+
+    print("Employee {} is done with tasks({}/{}):".format
+          (employee.get('name'), number_tasks, total_tasks))
+    for item in list_tasks:
+        print("\t" + item)
+
+
+    
