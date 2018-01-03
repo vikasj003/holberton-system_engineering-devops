a18210ae198578208ce561494ed1fe6f2cd3d3b4
wendysegura <118@holbertonschool.com>
2018-01-03 04:06:21 +0000

    not sure if working

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API"""
+import sys
+import json
+import requests
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    employee_id = sys.argv[1]
+
+    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId='
+    task_id = sys.argv[1]
+
+    employee_req = requests.get(url_user + employee_id)
+    employee = employee_req.json()
+    username = employee.get('username')
+
+    todos_req = requests.get(url_todos + task_id)
+    todos = todos_req.json()
+    csv_file = user_id + ".csv"
+
+    for item in todos:
+        complete = item.get('completed')
+        title = item.get('title')
+        with open(csv_file, "a+") as output:
+            output.write('"{}","{}", "{}", "{}"\n'.format
+                         (user_id, username, complete, title))
