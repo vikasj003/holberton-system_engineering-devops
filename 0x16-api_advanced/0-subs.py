eb122ab794bb1d864f9aaa4c107ab1a6b58ba734
wendysegura <118@holbertonschool.com>
2018-01-08 20:38:46 +0000

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """api subscribers"""
-import json
 import requests
 import sys
 
@@ -8,15 +7,14 @@ import sys
 def number_of_subscribers(subreddit):
     """subscribers"""
     subreddit = sys.argv[1]
-    #API call to reddit URL
-    url = "https://www.reddit.com/r/" + subreddit + "/about/.json"
-    #response object called "info_request" all info will be grabbed from this obj
-    info_request = requests.get(url, headers={'User-Agent': '118'})
-    
-    #to get keys in the "info_request" object
+    headers = {'user-agent': 'TankorSmash'}
+    """API call to reddit URL"""
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    """response object called "info_request" all info will be grabbed"""
+    info_request = requests.get(url, headers=headers)
     data = info_request.json().get("data")
-    subscribers = data.get("subscribers")
-    #If not a valid subreddit, return 0
-    if subscribers is None:
-        subscribers = 0
-    return(subscribers)
+    scribers = data.get("subscribers")
+    """If not a valid subreddit, return 0"""
+    if scribers is None:
+        scribers = 0
+    return(scribers)
