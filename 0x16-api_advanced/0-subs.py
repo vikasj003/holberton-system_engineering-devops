c0519fb596401ed142640cca7efec8164486cdff
wendysegura <118@holbertonschool.com>
2018-01-08 22:55:26 +0000

    upload missing return o for fake

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,12 @@ def number_of_subscribers(subreddit):
     """API call to reddit URL"""
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     """response object called "info_request" all info will be grabbed"""
-    info_request = requests.get(url, headers=headers)
-    data = info_request.json().get("data")
-    scribers = data.get("subscribers")
+    request = requests.get(url, headers=headers)
+    if not request:
+        return (0)
+    
+    data = request.json().get("data").get("subscribers")
     """If not a valid subreddit, return 0"""
-    if scribers is None:
-        scribers = 0
-    return(scribers)
+    if data is None:
+        data = 0
+    return(data)
