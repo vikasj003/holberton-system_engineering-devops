92950fac54cef8120d0a68d18399892259068317
wendysegura <118@holbertonschool.com>
2018-01-23 16:35:42 -0800

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'wordpress':
-  command     => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  path        => '/usr/bin:/usr/sbin:/bin',
+# fix 500 error 
+exec { 'wordpress typo error':
+  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
