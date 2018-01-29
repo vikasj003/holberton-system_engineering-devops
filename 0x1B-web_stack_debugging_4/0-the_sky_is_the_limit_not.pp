c429270a4273acc2eaa84741f9ed0061e126f170
wendysegura <118@holbertonschool.com>
2018-01-29 09:57:26 -0800

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #failed requests 
 exec { 'failed requests':
   command => "sudo sed -i 's/15/15000/' /etc/default/ngnix; sudo service nginx restart",
-  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin:/bin:/sbin',
 }
