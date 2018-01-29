5fa2f7c3ec58f30422b1dd7c472546fe26f845c4
wendysegura <118@holbertonschool.com>
2018-01-29 04:55:07 +0000

    upload

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+#failed requests 
+exec { 'failed requests':
+  command => "sudo sed -i 's/15/15000/' /etc/default/ngnix; sudo service nginx restart",
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin',
+}
